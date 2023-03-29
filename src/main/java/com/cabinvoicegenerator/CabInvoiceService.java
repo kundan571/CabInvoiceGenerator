19257ebca2296e57600f89655fa1029b1f6f99d9
kundan kumar <kundanchauhan1997@gmail.com>
2023-03-30 00:49:44 +0530

    Added: Test case should pass for given distance and time

--- src/main/java/com/cabinvoicegenerator/CabInvoiceService.java
+++ src/main/java/com/cabinvoicegenerator/CabInvoiceService.java
@@ -1,7 +1,16 @@
 package com.cabinvoicegenerator;
 
 public class CabInvoiceService {
+    private static final Double MINIMUM_COST_PER_KILOMETER = 10.0;
+    private static final int COST_PER_MINUTE = 1;
+
+    public Double calculateFare(Double distance, int time) {
+        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome To Cab Invoice Generator:!!");
+        CabInvoiceService invoiceService = new CabInvoiceService();
+        System.out.println(invoiceService.calculateFare(2.0,5));
     }
 }
--- src/test/java/com/cabinvoicegeneratortest/CabInvoiceServiceTest.java
+++ src/test/java/com/cabinvoicegeneratortest/CabInvoiceServiceTest.java
@@ -1,1 +1,18 @@
+package com.cabinvoicegeneratortest;
 
+import com.cabinvoicegenerator.CabInvoiceService;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInvoiceServiceTest {
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        CabInvoiceService invoiceService = new CabInvoiceService();
+        Double distance = 2.0;
+        int time = 5;
+        Double fare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+
+    }
+}
