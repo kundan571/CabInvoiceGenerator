54108c0b3f94c9a5718d94562790536b3bb03324
kundan kumar <kundanchauhan1997@gmail.com>
2023-03-30 00:43:15 +0530

    Added: Welcome message

---
+++ src/main/java/com/cabinvoicegenerator/CabInvoiceService.java
@@ -0,0 +1,7 @@
+package com.cabinvoicegenerator;
+
+public class CabInvoiceService {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Cab Invoice Generator:!!");
+    }
+}
