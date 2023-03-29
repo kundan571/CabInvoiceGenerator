336093e14c6aa00aaeb96df10d504434efe69978
kundan kumar <kundanchauhan1997@gmail.com>
2023-03-30 01:16:27 +0530

    Added: Test case should pass for the return minimum fare

--- src/main/java/com/cabinvoicegenerator/CabInvoiceService.java
+++ src/main/java/com/cabinvoicegenerator/CabInvoiceService.java
@@ -1,16 +1,22 @@
 package com.cabinvoicegenerator;
 
 public class CabInvoiceService {
-    private static final Double MINIMUM_COST_PER_KILOMETER = 10.0;
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_MINUTE = 1;
+    private static final double MINIMUM_FARE = 5;
 
-    public Double calculateFare(Double distance, int time) {
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+    public Double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        if (totalFare < MINIMUM_FARE) {
+            return MINIMUM_FARE;
+        } else {
+            return totalFare;
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome To Cab Invoice Generator:!!");
         CabInvoiceService invoiceService = new CabInvoiceService();
-        System.out.println(invoiceService.calculateFare(2.0,5));
+        System.out.println(invoiceService.calculateFare(2.0, 5));
     }
 }
--- src/test/java/com/cabinvoicegeneratortest/CabInvoiceServiceTest.java
+++ src/test/java/com/cabinvoicegeneratortest/CabInvoiceServiceTest.java
@@ -9,10 +9,18 @@ public class CabInvoiceServiceTest {
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         CabInvoiceService invoiceService = new CabInvoiceService();
-        Double distance = 2.0;
+        double distance = 2;
         int time = 5;
         Double fare = invoiceService.calculateFare(distance, time);
-        Assert.assertEquals(25, fare, 0.0);
+        Assert.assertEquals(25, fare, 0);
+    }
 
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare() {
+        CabInvoiceService invoiceService = new CabInvoiceService();
+        double distance = 0.1;
+        int time = 1;
+        Double fare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0);
     }
 }
